78d9e5092db334452004273787b1aa9e82beeb42
Steve McMahon <steve@dcn.org>
2014-02-22 16:06:08 -0800

    prototype some carousel styles to resemble the mockup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='diazotheme.plonecom',
       version=version,
       description="Theme for Plone.Com",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
